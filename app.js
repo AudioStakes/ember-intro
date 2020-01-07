c06c3027b4cc3ece48a1d029c308c83ca2ab5211
AudioStakes <daisuke.sato@enishi-tech.com>
2020-01-07 22:59:19 +0900

    intro 3rd Routing

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 App = Ember.Application.create();
+
+App.Router.map(function() {
+    this.resource('posts', function() {
+      this.route('show', {path: '/:post_id'});
+    });
+  });
+
+  App.Router.reopen({
+    location: 'none'
+  });
+  
+  // Route 定義
+  
+  App.PostsRoute = Ember.Route.extend({
+    model: function() {
+        return $.getJSON('http://emberjs.jsbin.com/goqene/2.json');
+        },
+    actions: {
+        error: function(jqXHR) {
+          if (jqXHR.status === 404) {
+            this.transitionTo('not_found');
+          } else {
+            this.transitionTo('something_went_wrong');
+          }
+        }
+    }
+  });
+  
+  App.PostsShowRoute = Ember.Route.extend({
+    model: function(params) {
+      var id = Number(params.post_id);
+      var posts = this.modelFor('posts');
+  
+      return posts.filter(function(post) {
+        return post.id === id;
+      })[0];
+    }
+  });
+
+  App.LoadingRoute = Ember.Route.extend({
+    activate: function() {
+      console.log('読み込み中です');
+    }
+  });
+
+  App.ErrorRoute = Ember.Route.extend({
+    activate: function() {
+      console.log('エラーです');
+    }
+  });
